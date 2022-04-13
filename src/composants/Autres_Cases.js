ffd7da970d24a18f9c6c2b4199e8e9b32b537994
ZephirSD <siegfrieddallery@gmail.com>
2022-04-13 20:12:22 +0400

    Création du composant de `Autres_Cases.js`

---
+++ src/composants/Autres_Cases.js
@@ -0,0 +1,34 @@
+import android from "../assets/icone/icon_android.png";
+import apple from "../assets/icone/icon_apple.png";
+
+function Autres() {
+    return ( 
+        <div className="padding-autres">
+            <section className="case-final-accueil">
+                <h1 className="titre-final-accueil">Et pleins d'autres</h1>
+                <div className="case-bloc-interface">
+                    <div className="case1"></div>
+                    <div className="case2"></div>
+                    <div className="case3"></div>
+                </div>
+            </section>
+            <div className="application-case">
+                <h2 className="titre-application">Disponible sur</h2>
+                <div className="case-boutton-appli">
+                    <div className="boutton-android">
+                        <img src={android} alt="icone_android"/>
+                    </div>
+                    <div className="boutton-apple">
+                        <img src={apple} alt="icone_apple"/>
+                    </div>
+                </div>
+            </div>
+            <div className="continue-case">
+                <h3 className="titre-continue">Continuer vers</h3>
+                <h1 className="text-contact">contact</h1>
+            </div>
+        </div>
+     );
+}
+
+export default Autres;
