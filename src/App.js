53cb7348305556dc46f084cfba99fcfdd38bfb93
ZephirSD <siegfrieddallery@gmail.com>
2022-04-13 20:12:57 +0400

    Ajout du composant `<Autres/>`

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@
 import './style/style.scss';
 import Accueil from './composants/Accueil';
 import Carte from './composants/Carte';
+import Autres from './composants/Autres_Cases';
 
 function App() {
   return (
     <div>
       <Accueil/>
       <Carte />
+      <Autres/>
     </div>
   );
 }
