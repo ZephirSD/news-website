d00499a5f6acd7193cf3baedf612aa1f8e38da20
ZephirSD <siegfrieddallery@gmail.com>
2022-04-15 20:25:59 +0400

    Insertion de la librairie AOS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 // import logo from './logo.svg';
 // import './App.css';
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import './style/style.scss';
 import Accueil from './composants/Accueil';
 import Carte from './composants/Carte';
@@ -7,12 +10,26 @@ import Autres from './composants/Autres_Cases';
 
 function App() {
   return (
-    <div>
+      useEffect(() => {
+      AOS.init();
+      AOS.refresh();
+    }),   
+    <>
+      {/* <div className='cursor-move'></div> */}
       <Accueil/>
       <Carte />
       <Autres/>
-    </div>
+    </>
   );
 }
 
 export default App;
+
+// let cursorMouse = document.querySelector('.cursor-move');
+// window.addEventListener('mousemove',moveCursor);
+
+// function moveCursor(e){
+//   cursorMouse.style.top = e.pageY + 'px';
+//   cursorMouse.style.left = e.pageX + 'px';
+// }
+
