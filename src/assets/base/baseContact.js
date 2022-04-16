287b355f1b5909e4b2d4c723e770376e96d79f56
ZephirSD <siegfrieddallery@gmail.com>
2022-04-16 12:49:52 +0400

    Création de la `baseContact` pour les infos de contact

---
+++ src/assets/base/baseContact.js
@@ -0,0 +1,18 @@
+import mail from "../../assets/icone-info-contact/mail_icon.png";
+import phone from "../../assets/icone-info-contact/phone_icon.png";
+import maps from "../../assets/icone-info-contact/maps_icon.png";
+
+export const baseContact = [
+	{
+		image_url: mail,
+        texte: 'contact@news.com'
+	},
+	{
+		image_url: phone,
+        texte: '0262 00 00 00'
+	},
+	{
+		image_url: maps,
+        texte: '3574 1st Lane, APT 213, Santa Fe, NM, 38679'
+	}
+]
