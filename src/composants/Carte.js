2e9033423c61dcd7da1b11b78199d42e0a7a1918
ZephirSD <siegfrieddallery@gmail.com>
2022-04-16 12:47:58 +0400

    Modification du composant `Carte` pour le backgroundImage de l'interface

--- src/composants/Carte.js
+++ src/composants/Carte.js
@@ -4,30 +4,17 @@ import deviceIphone from "../assets/device-iphone.png";
 // import { Mousewheel } from "swiper";
 import "swiper/css";
 import "swiper/css/effect-fade";
-// import interfaceNetwork from "../assets/interface_network.png";
 function Carte() {
   return (
     <div className="padding-carte">
-      {/* <Swiper
-        direction={"vertical"}
-        spaceBetween={0}
-        slidesPerView={1}
-        effect={"fade"}
-        // mousewheel={true}
-        mousewheel={{
-          forceToAxis: true,
-        }}
-        modules={[Mousewheel]}
-      > */}
-        {baseAccueil.map((base) => (
-        //   <SwiperSlide>
-            <section className="grid-carte">
+        {baseAccueil.map((base,index) => (
+            <section className="grid-carte" key={index}>
               <div className="case-carte-left">
                 <h1 className="titre-carte" data-aos="fade-up">{base.titre}</h1>
                 <p className="texte-carte" data-aos="fade-up">{base.texte}</p>
               </div>
               <div className="case-carte-right">
-                <div className="case-iphone" data-aos="zoom-in">
+                <div className="case-iphone" data-aos="zoom-in" style={{backgroundImage: `url(${base.url_image_phone})`}}>
                   <img
                     src={deviceIphone}
                     alt="device-iphone"
@@ -36,9 +23,7 @@ function Carte() {
                 </div>
               </div>
             </section>
-        //   </SwiperSlide>
         ))}
-      {/* </Swiper> */}
     </div>
   );
 }
