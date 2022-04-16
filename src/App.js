2624577b95185127054098ab60766fcc291cd0a5
ZephirSD <siegfrieddallery@gmail.com>
2022-04-16 12:43:04 +0400

    Création des Routes pour les différents composants

--- src/App.js
+++ src/App.js
@@ -1,35 +1,45 @@
 // import logo from './logo.svg';
 // import './App.css';
 import React, { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import './style/style.scss';
-import Accueil from './composants/Accueil';
-import Carte from './composants/Carte';
-import Autres from './composants/Autres_Cases';
+import "./style/style.scss";
+import Accueil from "./composants/Accueil";
+import Carte from "./composants/Carte";
+import Autres from "./composants/Autres_Cases";
+import Contact from "./composants/Contact";
+
+class Site extends React.Component{
+  render(){
+    return(
+      <>
+        <Accueil />
+        <Carte />
+        <Autres />
+      </>
+
+    );
+  }
+}
 
 function App() {
   return (
-      useEffect(() => {
+    useEffect(() => {
       AOS.init();
       AOS.refresh();
-    }),   
-    <>
-      {/* <div className='cursor-move'></div> */}
-      <Accueil/>
-      <Carte />
-      <Autres/>
-    </>
+    }),
+    (
+      <>
+        <Routes>
+          <Route>
+                <Route path="/" element={<Site/>} />
+                <Route path="/contact" element={<Contact/>} />
+          </Route>
+        </Routes>
+      </>
+    )
   );
 }
 
 export default App;
-
-// let cursorMouse = document.querySelector('.cursor-move');
-// window.addEventListener('mousemove',moveCursor);
-
-// function moveCursor(e){
-//   cursorMouse.style.top = e.pageY + 'px';
-//   cursorMouse.style.left = e.pageX + 'px';
-// }
-
