14fe4315909faa224563a214907a093e9212956e
ZephirSD <siegfrieddallery@gmail.com>
2022-04-16 12:45:24 +0400

    Insertion des images dans la table `baseAccueil`

--- src/assets/base/baseAccueilNews.js
+++ src/assets/base/baseAccueilNews.js
@@ -1,26 +1,29 @@
+import network from	"../../assets/interface_network.png";
+import meteo from	"../../assets/interface_meteo.png";
+
 export const baseAccueil = [
 	{
 		id: 1,
 		titre: 'network',
 		texte: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque risus augue, placerat non est non, scelerisque elementum nunc. Phasellus ante sapien, maximus in tempor quis, tempus eget magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin nec malesuada erat. Curabitur sed mollis ipsum, non maximus nulla. Pellentesque eu purus vel lectus elementum vulputate vel a lorem. Donec lacinia risus a ex convallis tempus. In posuere, felis sit amet maximus faucibus, metus est pellentesque sapien, et pulvinar tortor enim nec nisi. Maecenas facilisis enim sit amet massa dignissim faucibus.',
-        url_image_phone: 'interface_network.png'
+        url_image_phone: network
 	},
 	{
 		id: 2,
 		titre: 'entertainement',
 		texte: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque risus augue, placerat non est non, scelerisque elementum nunc. Phasellus ante sapien, maximus in tempor quis, tempus eget magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin nec malesuada erat. Curabitur sed mollis ipsum, non maximus nulla. Pellentesque eu purus vel lectus elementum vulputate vel a lorem. Donec lacinia risus a ex convallis tempus. In posuere, felis sit amet maximus faucibus, metus est pellentesque sapien, et pulvinar tortor enim nec nisi. Maecenas facilisis enim sit amet massa dignissim faucibus.',
-        url_image_phone: 'interface_network.png'
+        url_image_phone: network
 	},
 	{
 		id: 3,
 		titre: 'weather',
 		texte: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque risus augue, placerat non est non, scelerisque elementum nunc. Phasellus ante sapien, maximus in tempor quis, tempus eget magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin nec malesuada erat. Curabitur sed mollis ipsum, non maximus nulla. Pellentesque eu purus vel lectus elementum vulputate vel a lorem. Donec lacinia risus a ex convallis tempus. In posuere, felis sit amet maximus faucibus, metus est pellentesque sapien, et pulvinar tortor enim nec nisi. Maecenas facilisis enim sit amet massa dignissim faucibus.',
-        url_image_phone: 'interface_meteo.png'
+        url_image_phone: meteo
 	},
 	{
 		id: 4,
 		titre: 'sports',
 		texte: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque risus augue, placerat non est non, scelerisque elementum nunc. Phasellus ante sapien, maximus in tempor quis, tempus eget magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin nec malesuada erat. Curabitur sed mollis ipsum, non maximus nulla. Pellentesque eu purus vel lectus elementum vulputate vel a lorem. Donec lacinia risus a ex convallis tempus. In posuere, felis sit amet maximus faucibus, metus est pellentesque sapien, et pulvinar tortor enim nec nisi. Maecenas facilisis enim sit amet massa dignissim faucibus.',
-        url_image_phone: 'interface_network.png'
+        url_image_phone: network
 	}
 ]
