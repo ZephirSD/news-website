2f6cde88d0c5fb444e8080bb3070bf11eab9125c
ZephirSD <siegfrieddallery@gmail.com>
2022-04-18 11:50:02 +0400

    Modification du titre Sport

--- src/assets/base/baseAccueilNews.js
+++ src/assets/base/baseAccueilNews.js
@@ -22,7 +22,7 @@ export const baseAccueil = [
 	},
 	{
 		id: 4,
-		titre: 'sports',
+		titre: 'sport',
 		texte: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque risus augue, placerat non est non, scelerisque elementum nunc. Phasellus ante sapien, maximus in tempor quis, tempus eget magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin nec malesuada erat. Curabitur sed mollis ipsum, non maximus nulla. Pellentesque eu purus vel lectus elementum vulputate vel a lorem. Donec lacinia risus a ex convallis tempus. In posuere, felis sit amet maximus faucibus, metus est pellentesque sapien, et pulvinar tortor enim nec nisi. Maecenas facilisis enim sit amet massa dignissim faucibus.',
         url_image_phone: network
 	}
